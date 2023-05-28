b899420974e6fd639550f86ef0697cd57ff2d9f3
GitHub Action <action@github.com>
2023-05-28 13:03:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/wantsor/logseqqueryplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wantsor/logseqqueryplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.wantsor.logseqqueryplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wantsor.logseqqueryplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
